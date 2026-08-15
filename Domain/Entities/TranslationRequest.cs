b6124aa9e851fc04b8e54ed887f66bb68a9af686
fernandopelacini <fpela@hotmail.com>
2026-08-15 00:01:33 -0400

    Entities

---
+++ Domain/Entities/TranslationRequest.cs
@@ -0,0 +1,24 @@
+﻿
+using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class TranslationRequest
+    {
+        public int Id { get; set; }
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public string SourceText { get; set; } = "";
+        [StringLength(30)]
+        public string SourceLanguage { get; set; } = "";
+        [StringLength(30)]
+        public string TargetLanguage { get; set; } = "";
+        public TranslationStatus Status { get; set; }
+        public string? TranslatedText { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
