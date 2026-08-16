c5219bccd4fe3f25f28a6fe5b3b3d62a92821ecd
fernandopelacini <fpela@hotmail.com>
2026-08-16 00:16:34 -0400

    Command and handler for creating a translation

---
+++ Application/Commands/CreateTranslationRequest/CreateTranslationRequestCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.Commands.CreateTranslationRequest
+{
+    public class CreateTranslationRequestCommand : IRequest<int>
+    {
+        public int UserId { get; set; }
+        public int OrganizationId { get; set; }
+        public string SourceText { get; set; }
+        public string SourceLanguage { get; set; }
+        public string TargetLanguage { get; set; }
+    }
+}
