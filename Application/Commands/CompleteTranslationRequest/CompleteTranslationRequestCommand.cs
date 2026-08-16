bca8958660b5a255b31fa0664ec817c619debe5e
fernandopelacini <fpela@hotmail.com>
2026-08-16 00:16:10 -0400

    Command and handler for translation

---
+++ Application/Commands/CompleteTranslationRequest/CompleteTranslationRequestCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Commands.CompleteTranslationRequest
+{
+    public record CompleteTranslationRequestCommand(int RequestId, string TranslatedText) : IRequest<bool> 
+    {
+    }
+}
---
+++ Application/Commands/CompleteTranslationRequest/CompleteTranslationRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Commands.CompleteTranslationRequest
+{
+    public class CompleteTranslationRequestHandler : IRequestHandler<CompleteTranslationRequestCommand, bool>
+    {
+        private readonly ITranslationRequestRepository _repo;
+        public CompleteTranslationRequestHandler(ITranslationRequestRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<bool> Handle(CompleteTranslationRequestCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _repo.GetByIdAsync(request.RequestId, cancellationToken);
+            if (entity == null)
+                return false;
+
+            entity.TranslatedText = request.TranslatedText;
+            entity.Status = TranslationStatus.Completee;
+            entity.CompletedAt = DateTime.UtcNow;
+
+            await _repo.UpdateAsync(entity, cancellationToken);
+
+            return true;
+        }
+    }
+}
+
+
