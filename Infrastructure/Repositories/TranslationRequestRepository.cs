355324f686f2a8f4360448a728b332f4777c9f2b
fernandopelacini <fpela@hotmail.com>
2026-08-16 00:18:08 -0400

    Interfaces and the repository implementation of the interfaces

---
+++ Infrastructure/Repositories/TranslationRequestRepository.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class TranslationRequestRepository : ITranslationRequestRepository
+    {
+        private readonly AppDbContext _db;
+        public TranslationRequestRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task AddAsync(TranslationRequest request, CancellationToken cancellationToken)
+        {
+            _db.TranslationRequests.Add(request);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<TranslationRequest?> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            return await _db.TranslationRequests
+                .AsNoTracking()
+                .FirstOrDefaultAsync(r => r.Id == id, cancellationToken);
+        }
+
+        public async Task UpdateAsync(TranslationRequest request, CancellationToken cancellationToken)
+        {
+            _db.TranslationRequests.Update(request);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<List<TranslationRequest>> GetByUserIdAsync(int userId, CancellationToken cancellationToken)
+        {
+            return await _db.TranslationRequests
+                .AsNoTracking()
+                .Where(r => r.UserId == userId)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        public async Task CreateUserAsync(User request, CancellationToken cancellationToken)
+        {
+            _db.Users.Add(request);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<bool> UserExists(string username, string email, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email))
+            {
+                return true;
+            }
+
+            var user = await _db.Users.AsNoTracking().Where(u => u.UserName == username || u.Email == email).FirstOrDefaultAsync(cancellationToken);
+
+            if (user != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<User?> GetUserForLoginAsync(string identifier, CancellationToken cancellationToken)
+        {
+            return await _db.Users
+               .AsNoTracking()
+               .Where(u => u.UserName == identifier || u.Email == identifier)
+               .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            return computedHash.SequenceEqual(passwordHash);
+        }
+        public async Task<List<User>> GetUsersByOrganizationIdAsync(int organizationId, CancellationToken cancellationToken)
+        {
+            return await _db.Users
+               .AsNoTracking()
+               .Where(u => u.OrganizationId == organizationId)
+               .ToListAsync(cancellationToken);
+        }
+    }
+}
