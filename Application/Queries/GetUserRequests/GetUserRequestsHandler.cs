0669c0bfe831c22d6d9feea8b80dcd459c7f23b1
fernandopelacini <fpela@hotmail.com>
2026-08-16 00:19:22 -0400

    Query and handler for user requests

---
+++ Application/Queries/GetUserRequests/GetUserRequestsHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using MediatR;
+
+namespace Application.Queries.GetUserRequests
+{
+    public class GetUserRequestsHandler : IRequestHandler<GetUserRequestsQuery, List<TranslationRequestDto>>
+    {
+        private readonly ITranslationRequestRepository _repo;
+        public GetUserRequestsHandler(ITranslationRequestRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<List<TranslationRequestDto>> Handle(GetUserRequestsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repo.GetByUserIdAsync(request.UserId, cancellationToken);
+
+            return list.Select(entity => new TranslationRequestDto(
+            entity.Id,
+            entity.UserId,
+            entity.OrganizationId,
+            entity.SourceText,
+            entity.SourceLanguage,
+            entity.TargetLanguage,
+            entity.TranslatedText,
+            entity.Status,
+            entity.CreatedAt,
+            entity.CompletedAt
+        )).ToList();
+        }
+    }
+}
