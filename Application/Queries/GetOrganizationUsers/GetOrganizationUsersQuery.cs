90d4bb3ef7dbcb45666f45bde9c483da6228a7e5
fernandopelacini <fpela@hotmail.com>
2026-08-16 00:18:36 -0400

    Query and handler for organization for user

---
+++ Application/Queries/GetOrganizationUsers/GetOrganizationUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.DTOs;
+using MediatR;
+
+namespace Application.Queries.GetOrganizationUsers
+{
+    public record GetOrganizationUsersQuery(int OrganizationId) : IRequest<List<UserDto>>;
+}
