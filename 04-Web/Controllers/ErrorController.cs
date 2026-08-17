4140a89049ad9e626b18f7f2048545ec0941dc10
fernandopelacini <fpela@hotmail.com>
2026-08-17 19:13:07 -0400

    New controllers and views to manage errors

---
+++ 04-Web/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04_Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HandleError(int statusCode)
+        {
+            return statusCode switch
+            {
+                404 => View("404"),
+                403 => View("403"),
+                _ => View("500")
+            };
+        }
+    }
+}
