12131c796dd98f77c5acdb03806ffcf591b8ad4b
fernandopelacini <fpela@hotmail.com>
2026-08-17 19:19:03 -0400

    New column to capture processing date for a translation

---
+++ Infrastructure/Migrations/20260816224338_AddTranslationProcessingDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _03.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTranslationProcessingDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProcessingStartedAt",
+                table: "TranslationRequests",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingStartedAt",
+                table: "TranslationRequests");
+        }
+    }
+}
