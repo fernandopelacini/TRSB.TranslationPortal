1932416982e4362ae5d3293f116e9a9d540ffeaa
fernandopelacini <fpela@hotmail.com>
2026-08-17 19:15:05 -0400

    Helper to show a flag by the language name

---
+++ 04-Web/Helpers/FlagHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace _04_Web.Helpers
+{
+    public static class FlagHelper
+    {
+        public static readonly Dictionary<string, string> Flags = new()
+        {
+            { "English", "🇬🇧" },
+            { "French", "🇫🇷" },
+            { "Spanish", "🇪🇸" },
+            { "German", "🇩🇪" },
+            { "Italian", "🇮🇹" }
+        };
+    }
+}
