206d397e67bb185736ac8cf204c41d7f2d8575a9
fernandopelacini <fpela@hotmail.com>
2026-08-17 19:50:20 -0400

    Engine selector tests

---
+++ TRSB.TranslationPortal.Tests/Application.tests/TranslationEngineSelectorTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Moq;
+
+namespace TRSB.TranslationPortal.Tests.Application.tests
+{
+    public class TranslationEngineSelectorTests
+    {
+        [Fact]
+        public void SelectEngine_Should_Return_One_Of_The_Engines()
+        {
+            // Arrange
+            var e1 = new Mock<ITranslationEngine>().Object;
+            var e2 = new Mock<ITranslationEngine>().Object;
+            var selector = new TranslationEngineSelector(new[] { e1, e2 });
+
+            // Act
+            var engine = selector.SelectEngine();
+
+            // Assert
+            Assert.Contains(engine, new[] { e1, e2 });
+        }
+
+        [Fact]
+        public void SelectEngine_Should_Not_Return_Null()
+        {
+            var e1 = new Mock<ITranslationEngine>().Object;
+            var selector = new TranslationEngineSelector(new[] { e1 });
+
+            var engine = selector.SelectEngine();
+
+            Assert.NotNull(engine);
+        }
+    }
+}
